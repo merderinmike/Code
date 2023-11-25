5da817772b56d58780159ad33c322ff600c9cc19
merderinmike <alecshehorn97@gmail.com>
2023-11-25 15:08:49 -0600

    Initial commit

---
+++ js/home/index.js
@@ -0,0 +1,30 @@
+const url = "http://localhost:3000/api/products";
+const container = document.getElementById("items");
+fetch(url)
+	.then((response) => response.json())
+	.then((data) => {
+		for (let {
+			name,
+			price,
+			description,
+			imageUrl,
+			altTxt,
+			colors,
+		} of data) {
+			container.innerHTML += `
+            <article>
+            <img
+                src="${imageUrl}"
+                alt="${altTxt}"
+            />
+            <h3 class="productName">${name}</h3>
+            <p class="productDescription">
+                ${description}
+            </p>
+        </article>
+            `;
+		}
+	})
+	.catch((error) => {
+		console.error("error fetching data: ", error);
+	});
